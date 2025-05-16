7e947dfed766a4d5a09ac722a5c2a13b97b8a847
spaikid666 <iphonedealvarosalguero@gmail.com>
2025-05-16 21:19:41 +0200

    close unused FDs

--- pipex.c
+++ pipex.c
@@ -18,15 +18,14 @@ static void	child_process(int *fd, char **argv, char **envp)
 
 	fd_in = open(argv[1], O_RDONLY, 0777);
 	if (fd_in == -1)
-	{
-		perror("Error, couldn't open input file");
-		return (exit(1));
-	}
-	if (dup2(fd_in, STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
-	{
-		perror("Error in setting file descriptor in the child");
-		return (exit(1));
-	}
+		return (perror("Error, couldn't open input file"), \
+		close(fd[1]), exit(1));
+	if (dup2(fd_in, STDIN_FILENO) == -1)
+		return (perror("Error, couldn't open input file"), close(fd[1]), \
+		close(fd_in), exit(1));
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		return (perror("Error, couldn't open input file"), close(fd[1]), \
+		exit(1));
 	close(fd[0]);
 	if (argv[2] == NULL || argv[2][0] == '\0')
 	{
@@ -44,16 +43,20 @@ static void	parent_process(int *fd, char **argv, char **envp)
 	waitpid(-1, &status, 0);
 	fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd_out == -1)
-	{
-		perror("Error, couldn't open output file");
-		return (exit(1));
-	}
-	if (dup2(fd[0], STDIN_FILENO) == -1 || dup2(fd_out, STDOUT_FILENO) == -1)
-	{
-		perror("Error in setting the file descriptor in the parent");
-		return (exit(1));
-	}
+		return (perror("Error, couldn't open input file"), \
+		close(fd[1]), exit(1));
+	if (dup2(fd_out, STDIN_FILENO) == -1)
+		return (perror("Error, couldn't open input file"), close(fd[0]), \
+		close(fd_out), exit(1));
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		return (perror("Error, couldn't open input file"), close(fd[0]), \
+		exit(1));
 	close(fd[1]);
+	if (argv[4] == NULL || argv[4][0] == '\0')
+	{
+		perror("Error, the command is invalid");
+		return (exit(1));
+	}
 	ft_execute_cmd(argv[3], envp);
 }
 
@@ -65,19 +68,20 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 5)
 		return (perror(INVALID_ARGS), -1);
 	if (pipe(fd) == -1)
-	{
-		perror("The pipe creation failed");
-		return (-1);
-	}
+		return (perror("The pipe creation failed"), -1);
 	pid = fork();
 	if (pid == -1)
-	{
-		perror("Error forking the process");
-		return (-1);
-	}
+		return (perror("Error forking the process"), close(fd[0]), \
+		close(fd[1]), -1);
 	if (pid == 0)
+	{
+		close(fd[0]);
 		child_process(fd, argv, envp);
+	}
 	else
+	{
+		close(fd[1]);
 		parent_process(fd, argv, envp);
+	}
 	return (0);
 }
